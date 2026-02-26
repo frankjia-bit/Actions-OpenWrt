eed2d59a63700b2ba89e3cd757c64e07dc387cbc
frankjia-bit <14528755@qq.com>
2026-02-27 01:26:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,26 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#!/bin/bash
+# diy-part1.sh - 添加CUPS第三方库
+
+echo "添加CUPS打印服务器第三方库"
+
+# 1. 添加CUPS专用feed到feeds.conf.default
+echo 'src-git cups https://github.com/Gr4ffy/lede-cups.git' >> feeds.conf.default
+
+# 2. 更新所有feeds（包括新添加的CUPS feed）
+./scripts/feeds update -a
+
+# 3. 安装CUPS相关包
+./scripts/feeds install cups
+./scripts/feeds install luci-app-cups
+./scripts/feeds install cups-bjnp
+./scripts/feeds install cups-filters
+
+# 4. 安装依赖包
+./scripts/feeds install avahi-daemon
+./scripts/feeds install avahi-dbus-daemon
+./scripts/feeds install kmod-usb-printer
+
+echo "CUPS第三方库添加完成"
